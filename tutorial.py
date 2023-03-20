d84c36476d0a2e3c7cfe712d72b2f965f263f70b
JBN <128038111+Joe-BN@users.noreply.github.com>
2023-03-20 08:40:51 +0300

    Add files via upload

---
+++ tutorial.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost"
+    user="root"
+    passwd="root"
+    database="testdatabase"
+)
+
+mycursor = db.cursor
+mycursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
+mycursor.execute("DESCRIBE Person (name, age) VALUES (%s,%s)", ("Joseph", 17))
+db.commit()
+
+mycursor.execute("SELECT * FROM Person")
+
+for x in mycursor:
+    print (x)
