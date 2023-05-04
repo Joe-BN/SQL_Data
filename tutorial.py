eb847c35ab15326cd72c395fded4cd53662e2a2e
JBN <128038111+Joe-BN@users.noreply.github.com>
2023-05-04 12:28:40 +0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -7,6 +7,7 @@ db = mysql.connector.connect(
     database="testdatabase"
 )
 
+
 mycursor = db.cursor
 mycursor.execute("CREATE TABLE Person (name VARCHAR(50), age smallint UNSIGNED, personID int PRIMARY KEY AUTO_INCREMENT)")
 mycursor.execute("DESCRIBE Person (name, age) VALUES (%s,%s)", ("Joseph", 17))
